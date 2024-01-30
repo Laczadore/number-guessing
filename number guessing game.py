4c972de8a86baa51a56ecedfba5330f9e2964643
dawid <dawidfalinski14@gmail.com>
2024-01-30 11:06:21 +0100

    added feature which allows user to input how many chances he wants to have

--- number guessing game.py
+++ number guessing game.py
@@ -1,11 +1,11 @@
 import random
 
-max_range = int(input('configure max number[range = 1 to your number]'))
+max_range = int(input('configure max number[range is from 1 to your number] '))
 
 number = random.choice(range(1, max_range + 1))
 print(number)
 
-chances = 3
+chances = int(input('how many chances you want to have?'))
 while True:
     guess = int(input(f'guess the number, from 1 to {max_range} \n'))
     if guess == number:
@@ -13,5 +13,13 @@ while True:
         break
     elif guess > number:
         print('Bad guess. The number you guessed is too high \n')
+        chances = chances - 1
+        print(f'you have {chances} chances left')
     elif guess < number:
         print('Bad guess. The number you guessed is too low \n')
+        chances = chances - 1
+        print(f'you have {chances} chances left')
+    if chances == 0:
+        print(f'you lost because you ran out of the chances :/ number was {number}')
+        break
+    
